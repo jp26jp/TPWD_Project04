3b96b04e50b49cba761e8319be6ea5a040fb04f1
John R Perry <john@j-26.com>
2019-03-04 03:29:05 -0700

    Finished fixing project issues

--- task.py
+++ task.py
@@ -9,7 +9,7 @@ long_separator_string = "{}".format("=" * 60)
 
 
 class Task(Model):
-    employee_name = TextField()
+    employee = TextField()
     task_name = TextField()
     time_worked = IntegerField(default=0)
     task_notes = TextField()
@@ -25,75 +25,60 @@ class TaskManager:
         """Create the database and the table if they don't exist"""
         db.connect(reuse_if_open=True)
         db.create_tables([Task], safe=True)
-        menu_text = self.generate_menu_text(self.main_menu, "\nWhat would you like to do?")
-        selection = None
-        while selection != 'q':
-            selection = self.request_int(menu_text, _max=len(self.main_menu) - 1)
-            self.main_menu[selection](self)
+        self.display_menu(self.main_menu, "\nWhat would you like to do?")
 
-    @staticmethod
-    def request_int(message="\nEnter selection: ", _max=-1):
+    def request_int(self, message="\nEnter selection: ", _max=-1):
         while True:
             try:
-                input_value = input(message)
+                input_value = input(message).lower()
                 if input_value == "q":
-                    break
+                    exit()
                 input_value = int(input_value)
                 if _max != -1 and input_value > _max:
-                    print("\nOnly numbers between 0 and {}".format(_max))
+                    raise UserWarning("\nOnly numbers between 0 and {}".format(_max))
                 else:
-                    break
+                    return input_value
             except ValueError:
                 print("\nNumbers only")
-        return input_value
 
     def find_tasks_with_employee_name(self) -> [Task]:
         """find by employee"""
-        menu_text = self.generate_menu_text(self.search_by_employee, "\nEnter a search method:")
-        selection = self.request_int(menu_text, _max=len(self.search_by_employee) - 1)
-        tasks = self.search_by_employee[selection](self)
-        return tasks
+        return self.display_menu(self.search_by_employee)
 
     def find_tasks_with_employee_name_from_list(self) -> [Task]:
         """choose from employee list"""
-        employees = Task.select(Task.employee_name).distinct()
+        employees = Task.select(Task.employee).distinct()
         print("\nSelect an employee:\n")
         for i, employee in enumerate(employees):
             print("[{}] {}".format(i, employee.employee_name))
         employee = self.request_int(_max=len(employees) - 1)
-        return Task.select().where(Task.employee_name == employees[employee].employee_name)
+        return Task.select().where(Task.employee == employees[employee].employee_name)
 
     def find_tasks_with_employee_name_from_search(self) -> [Task]:
         """manually enter name"""
         employee_name = input("\nEnter employee name: ")
-        return Task.select().where(Task.employee_name.contains(employee_name))
+        return Task.select().where(Task.employee.contains(employee_name))
 
     def find_tasks_with_date(self) -> [Task]:
         """find by date"""
         tasks = Task.select(Task.timestamp).distinct()
         if tasks:
-            print(short_separator_string)
-            print("\nDates available:\n")
+            print("{}\nDates available:\n".format(short_separator_string))
             for i, task in enumerate(tasks):
                 print("[{}]: {}".format(i, task.timestamp))
             date = self.request_int(_max=len(tasks) - 1)
-            tasks = Task.select().order_by(Task.timestamp.asc()).where(
-                Task.timestamp == tasks[date].timestamp)
+            tasks = Task.select().order_by(Task.timestamp.asc()).where(Task.timestamp == tasks[date].timestamp)
         return tasks
 
     def find_tasks_with_time_spent(self) -> [Task]:
         """find by time spent"""
-        print(short_separator_string)
-        time_spent = self.request_int("\nEnter time spent in minutes: ")
+        time_spent = self.request_int("{}\nEnter time spent in minutes: ".format(short_separator_string))
         return Task.select().where(Task.time_worked == time_spent)
 
     def find_tasks_with_search(self) -> [Task]:
         """find with search term"""
-        print(short_separator_string)
-        query = input("\nEnter search query: ")
-        tasks = Task.select().where(
-            (Task.task_name.contains(query)) | (Task.task_notes.contains(query))
-        )
+        query = input("{}\nEnter search query: ".format(short_separator_string))
+        tasks = Task.select().where((Task.task_name.contains(query)) | (Task.task_notes.contains(query)))
         return tasks
 
     def add_task(self) -> bool:
@@ -102,56 +87,61 @@ class TaskManager:
         task_name = input("Task name: ")
         time_worked = self.request_int("Time spent in minutes: ")
         task_notes = input("Task notes: ")
-
         if input("\nSave task? [Yn]: ").lower() != "n":
-            return self.save_task(employee_name, task_name, time_worked,
-                                  task_notes)
+            return self.save_task(employee_name, task_name, time_worked, task_notes)
         return False
 
-    @staticmethod
-    def save_task(employee_name: str, task_name: str, time_worked: int,
-                  task_notes: str) -> bool:
-        if isinstance(employee_name, str) and \
-                isinstance(task_name, str) and \
-                isinstance(time_worked, int) and \
-                isinstance(task_notes, str):
-            Task.create(employee_name=employee_name,
-                        time_worked=time_worked,
-                        task_name=task_name,
-                        task_notes=task_notes)
+    def save_task(self, employee: str, task_name: str, time_worked: int, task_notes: str) -> bool:
+        if self.isstr(employee) and self.isstr(task_name) and self.isint(time_worked) and self.isstr(task_notes):
+            Task.create(employee=employee, task_name=task_name, time_worked=time_worked, task_notes=task_notes)
             print("\nSaved!\n")
             return True
         return False
 
     def lookup_tasks(self):
         """view previous tasks"""
-        menu_text = self.generate_menu_text(self.search_menu, "\nEnter a search method:")
-        selection = self.request_int(menu_text, _max=len(self.search_menu) - 1)
-        tasks = self.search_menu[selection](self)
+        tasks = self.display_menu(self.search_menu)
         if not len(tasks):
             print("\nNo tasks available\n")
         else:
-            for task in tasks:
-                print(long_separator_string)
-                self.print_task(task)
-            print("")
+            self.print_tasks(tasks)
 
-    def print_task(self, task):
-        print("\n{}".format(task.timestamp))
-        print(long_separator_string)
-        print("Employee name:   {}\n"
-              "Task name:       {}\n"
-              "Time spent:      {}\n"
-              "Task notes:      {}".format(task.employee_name, task.task_name,
-                                           task.time_worked, task.task_notes))
+    def print_tasks(self, tasks):
+        for task in tasks:
+            self.generate_task_string(task)
+        print("")
 
     @staticmethod
-    def generate_menu_text(menu, menu_text="\nEnter selection:") -> str:
+    def generate_task_string(task: Task) -> str:
+        task_string = "\n{}\n{}\n" \
+                      "Employee name:  {}\n" \
+                      "Task name:      {}\n" \
+                      "Time spent:     {}\n" \
+                      "Task notes:     {}\n" \
+                      "{}".format(task.timestamp, long_separator_string, task.employee, task.task_name,
+                                  task.time_worked, task.task_notes, long_separator_string)
+        return task_string
+
+    @staticmethod
+    def generate_menu_text(menu, menu_text) -> str:
         menu_text = menu_text + "\n"
         for key, value in menu.items():
             menu_text = menu_text + "\n[{}]: {}".format(key, value.__doc__)
         menu_text = menu_text + "\n\nEnter selection: "
         return menu_text
+
+    def display_menu(self, menu, message="\nEnter a search method:"):
+        menu_text = self.generate_menu_text(menu, message)
+        selection = self.request_int(menu_text, _max=len(self.main_menu) - 1)
+        return menu[selection](self)
+
+    @staticmethod
+    def isstr(string):
+        return isinstance(string, str)
+
+    @staticmethod
+    def isint(integer):
+        return isinstance(integer, int)
 
     main_menu = OrderedDict([
         (0, add_task),
@@ -169,7 +159,3 @@ class TaskManager:
         (0, find_tasks_with_employee_name_from_list),
         (1, find_tasks_with_employee_name_from_search),
     ])
-
-
-if __name__ == '__main__':
-    TaskManager()
--- test_taskManager.py
+++ test_taskManager.py
@@ -1,34 +1,66 @@
-from unittest import TestCase
-
+from collections import OrderedDict
+from unittest.mock import patch
+from task import TaskManager
+from playhouse.sqlite_ext import *
+import unittest
+import string
+import random
 import task
 
+test_db = SqliteDatabase(':memory:')
 
-class TestTaskManager(TestCase):
+
+class TestTaskManager(unittest.TestCase):
     def test_request_int(self):
-        input_value = task.TaskManager.request_int()
-        self.assertIs(type(input_value), int)
-
-    def test_add_task(self):
-        new_task = task.TaskManager.add_task(task.TaskManager)
-        self.assertTrue(new_task)
+        inputs = [
+            1,
+            random.choice(string.ascii_lowercase).replace("q", ""),
+            "q",
+            5
+        ]
+        with patch("builtins.input", side_effects=inputs):
+            # test 1
+            value = task.TaskManager.request_int(task)
+            self.assertTrue(isinstance(value, int))
+            # test random letter
+            TaskManager.request_int(task)
+            self.assertRaises(ValueError)
+            # test q
+            TaskManager.request_int(task)
+            self.assertRaises(SystemExit)
+            # test value > _max
+            TaskManager.request_int(task, 4)
+            self.assertRaises(UserWarning)
 
     def test_save_task(self):
-        bad_task = task.TaskManager.save_task("", "", "", "")
-        self.assertFalse(bad_task)
-        good_task = task.TaskManager.save_task("John", "Work", 10, "Notes")
-        self.assertTrue(good_task)
+        self.assertTrue(TaskManager.save_task(task.TaskManager, "Bob", "Stuff", 10, "None"))
 
-    def test_display_menu(self):
-        from collections import OrderedDict
-        menu = OrderedDict([(0, self.dummy_method)])
-        menu_string = task.TaskManager.generate_menu_text(menu).replace('\n', '')
-        test_result = 'Enter selection:[0]: dummy method for display_menu()Enter selection: '
-        self.assertEqual(menu_string, test_result)
+    def test_generate_menu_text(self):
+        menu = OrderedDict([(0, self.dummy_method), (1, self.dummy_method2)])
+        menu_text = TaskManager.generate_menu_text(menu, "")
+        test_menu_text = "\n\n[0]: a super dumb method\n[1]: an even dumber method\n\nEnter selection: "
+        self.assertEqual(menu_text, test_menu_text)
+
+    def test_generate_task_string(self):
+        expected_string = "\n2019-03-04\n" \
+                          "============================================================\n" \
+                          "Employee name:  Bob\n" \
+                          "Task name:      Stuff\n" \
+                          "Time spent:     10\n" \
+                          "Task notes:     None\n" \
+                          "============================================================"
+        task_string = TaskManager.generate_task_string(task.Task(employee="Bob", task_name="Stuff", time_worked=10,
+                                                                 task_notes="None", timestamp="2019-03-04"))
+        self.assertEqual(expected_string, task_string)
+
+    def test_isstr(self):
+        self.assertTrue(TaskManager.isstr("string"))
+
+    def test_isint(self):
+        self.assertTrue(TaskManager.isint(1))
 
     def dummy_method(self):
-        """dummy method for display_menu()"""
-        pass
+        """a super dumb method"""
 
-    def test_print_task(self):
-        task.TaskManager.print_task(task.TaskManager,
-                                    task.Task("Bob", "Did stuff", 10, "Notes", "2019-02-25"))
+    def dummy_method2(self):
+        """an even dumber method"""
